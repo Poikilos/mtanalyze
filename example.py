c567f7d9cd57e6ffe5fc7418bf8ebf0c30caa310
poikilos <7557867+poikilos@users.noreply.github.com>
2021-01-31 18:02:15 -0500

    Add an example.

---
+++ example.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+from __future__ import print_function
+'''
+MIP is for "Minetest [Run] In-place". That means this will only generate
+documentation for games in "$HOME/minetest/games" (or USERPROFILE if
+the environment variable is present).
+'''
+import sys
+import os
+
+from mtanalyze.minetestinfo import mti
+
+print()
+me = os.path.split(__file__)[-1]
+shared_minetest_path = mti.get_var('shared_minetest_path')
+profile_minetest_path = mti.get_var('profile_minetest_path')
+if shared_minetest_path is not None:
+    if shared_minetest_path == profile_minetest_path:
+        print('RUN_IN_PLACE is apparently true (profile_minetest_path'
+              ' matches shared_minetest_path).')
+    else:
+        print('RUN_IN_PLACE is apparently false (profile_minetest_path'
+              ' differs from shared_minetest_path).')
+else:
+    print('RUN_IN_PLACE is unknown since shared_minetest_path is'
+          ' not known.')
+print()
+print("[ {} ] mti.get_var() results for name in"
+      " mti.keys():".format(me))
+print()
+# checkNames = ["minetestserver_path"]
+checkNames = mti.keys()
+for name in checkNames:
+    if mti.contains(name):
+        print("{}: {}".format(name, mti.get_var(name)))
+    else:
+        print("{} is not known.".format(name))
+
+print()
+print("[ {} ] done.".format(me))
